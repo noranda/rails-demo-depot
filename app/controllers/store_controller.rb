4ac7518b1dc383bb14a68bae7ac968199264f45d
Noranda Brown <noranda@norandabrown.com>
2014-02-28 17:24:57 -0500

    completed chapter 9

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
+  end
+
   def index
+    @count = increment_counter
     @products = Product.all
   end
 end
