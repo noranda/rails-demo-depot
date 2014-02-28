e383895b40dfcef19796acb49ab5030b1a733e63
Noranda Brown <noranda@norandabrown.com>
2014-02-28 12:43:03 -0500

    completed chapter 8

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_description', :minimum => 4
+    assert_select '.list_actions', :minimum => 4
   end
 
   test "should get new" do
